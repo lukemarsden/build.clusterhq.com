9e329d147772c48271195fae9a3ee51bd6302c5c
Luke Marsden <luke@clusterhq.com>
2015-05-04 23:45:16 +0100

    wip on ubuntuification

--- slave/vagrant/fabfile.py
+++ slave/vagrant/fabfile.py
@@ -6,10 +6,12 @@ Configuration for a buildslave to run vagrant on.
     This points at the labs buildserver by default.
 """
 
-from fabric.api import run, task, sudo, put
+from fabric.api import run, task, sudo, put, env
 from twisted.python.filepath import FilePath
 from StringIO import StringIO
 import yaml
+
+env.user = "root"
 
 
 def configure_acceptance():
@@ -22,8 +24,9 @@ def install(index, password, master='build.labs.clusterhq.com'):
     """
     Install a buildslave with vagrant installed.
     """
-    run("apt-get install -y vagrant virtualbox virtualbox-dkms")
-    run("useradd buildslave")
+    run("apt-get install -y virtualbox virtualbox-dkms buildbot-slave")
+    run("wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb && dpkg -i vagrant_1.7.2_x86_64.deb")
+    run("useradd buildslave || true")
     sudo("buildslave create-slave /home/buildslave/fedora-vagrant %(master)s fedora-vagrant-%(index)s %(password)s"
          % {'index': index, 'password': password, 'master': master},
          user='buildslave')
