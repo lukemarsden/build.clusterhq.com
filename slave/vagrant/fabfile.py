2f2d37beb1d50693f35b60c643e673317627997f
Luke Marsden <luke@clusterhq.com>
2015-05-04 23:36:23 +0100

    smash into ubuntu land

--- slave/vagrant/fabfile.py
+++ slave/vagrant/fabfile.py
@@ -3,31 +3,13 @@
 Configuration for a buildslave to run vagrant on.
 
 .. warning::
-    This points at the staging buildserver by default.
+    This points at the labs buildserver by default.
 """
 
-from fabric.api import run, task, sudo, put, local
+from fabric.api import run, task, sudo, put
 from twisted.python.filepath import FilePath
 from StringIO import StringIO
 import yaml
-
-
-def get_vagrant_config():
-    output = local('lpass show --notes "vagrant@build.clusterhq.com"',
-                   capture=True)
-    config = yaml.safe_load(output.stdout)
-    return config
-
-
-def configure_gsutil(config):
-    """
-    Install certificate and configuration for gsutil.
-
-    This allows the slave to upload vagrant images to google cloud.
-    """
-    boto_config = FilePath(__file__).sibling('boto-config.in').getContent()
-    put(StringIO(boto_config % config), '/home/buildslave/.boto')
-    put(StringIO(config['certificate']), '/home/buildslave/google.p12')
 
 
 def configure_acceptance():
@@ -40,36 +22,9 @@ def install(index, password, master='build.labs.clusterhq.com'):
     """
     Install a buildslave with vagrant installed.
     """
-    config = get_vagrant_config()
-
-    run("wget -O /etc/yum.repos.d/virtualbox.repo http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo")  # noqa
-
-    run("""
-UNAME_R=$(uname -r)
-PV=${UNAME_R%.*}
-KV=${PV%%-*}
-SV=${PV##*-}
-ARCH=$(uname -m)
-yum install -y https://kojipkgs.fedoraproject.org//packages/kernel/${KV}/${SV}/${ARCH}/kernel-devel-${UNAME_R}.rpm  # noqa
-""")
-
-    run('yum install -y dkms')
-    packages = [
-        "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.5_x86_64.rpm",
-        "VirtualBox-4.3.x86_64",
-        "https://kojipkgs.fedoraproject.org//packages/buildbot/0.8.10/1.fc22/noarch/buildbot-slave-0.8.10-1.fc22.noarch.rpm",  # noqa
-        "mongodb",
-        "git",
-        "libffi-devel",
-        "python",
-        "python-devel",
-        "python-virtualenv",
-        "openssl-devel",
-        "https://clusterhq.s3.amazonaws.com/phantomjs-fedora-20/phantomjs-1.9.8-1.x86_64.rpm",  # noqa
-    ]
-    run("yum install -y " + " ".join(packages))
+    run("apt-get install -y vagrant virtualbox virtualbox-dkms")
     run("useradd buildslave")
-    sudo("buildslave create-slave /home/buildslave/fedora-vagrant %(master)s fedora-vagrant-%(index)s %(password)s"  # noqa
+    sudo("buildslave create-slave /home/buildslave/fedora-vagrant %(master)s fedora-vagrant-%(index)s %(password)s"
          % {'index': index, 'password': password, 'master': master},
          user='buildslave')
     put(FilePath(__file__).sibling('start').path,
@@ -78,16 +33,8 @@ yum install -y https://kojipkgs.fedoraproject.org//packages/kernel/${KV}/${SV}/$
     sudo("vagrant plugin install vagrant-reload vagrant-vbguest",
          user='buildslave')
     configure_acceptance()
-    configure_gsutil(config=config['google-certificate'])
 
-    put(FilePath(__file__).sibling('fedora-vagrant-slave.service').path,
-        '/etc/systemd/system/fedora-vagrant-slave.service')
+    put(FilePath(__file__).sibling('vagrant-slave.conf').path,
+        '/etc/init/vagrant-slave.conf')
 
-    run('systemctl start fedora-vagrant-slave')
-    run('systemctl enable fedora-vagrant-slave')
-
-
-@task
-def update_config():
-    config = get_vagrant_config()
-    configure_gsutil(config=config['google-certificate'])
+    run('start vagrant-slave')
